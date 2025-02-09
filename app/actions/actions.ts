d6a3cf09bef225252b808de82bf84c4c7bba7e42
Elliott Tuan <gbs.tuan@gmail.com>
2025-02-09 09:58:59 +0800

    feat: Add add task function to AddTodoForm

---
+++ app/actions/actions.ts
@@ -0,0 +1,36 @@
+"use server"
+
+import { revalidatePath } from "next/cache";
+
+const wayiAPI = "https://wayi.league-funny.com/api"
+
+export async function addTask(prevState: string | null | undefined, formData: FormData) {
+
+    const name = formData.get("name") as string;
+    const description = formData.get("description") as string;
+    const currentTime = new Date().toISOString();
+
+    const task = {
+        "name": name,
+        "description": description,
+        "is_completed": false,
+        "created_at": currentTime,
+        "updated_at": currentTime
+    }
+
+    try {
+        await fetch(`${wayiAPI}/task`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(task),
+        });
+
+        revalidatePath("/")
+    } catch (error) {
+        if (error instanceof Error) {
+            return error.message
+        }
+    }
+}
--- components/AddTodoForm.tsx
+++ components/AddTodoForm.tsx
@@ -1,36 +1,49 @@
+"use client";
+
+import { addTask } from "@/app/actions/actions";
+import { useActionState } from "react";
+
 export default function AddTodoForm() {
+  const [error, action, isPending] = useActionState(addTask, null);
+
   return (
-    <form className="border bg-base-200 border-gray-700 shadow-xl rounded-md">
+    <form
+      action={action}
+      className="border bg-base-200 border-gray-700 shadow-xl rounded-md"
+    >
       <div className="flex flex-col gap-4 justify-center items-center p-8 px-12">
         <div className="w-full">
-          <label htmlFor="name">Name</label>
+          <label htmlFor="name">
+            Name<span className="ml-4 text-xs text-green-600">*required</span>
+          </label>
           <input
             required
             id="name"
             name="name"
             type="text"
             maxLength={10}
-            className="bg-base-200 p-2 pl-4 mt-2 border border-gray-600 rounded-md w-full focus:outline-2 focus:outline-gray-500 focus:-outline-offset-1"
+            className="bg-base-200 p-2 pl-4 mt-2 w-full border border-gray-600 focus:outline-2 focus:outline-gray-500 focus:-outline-offset-1 rounded-md"
           />
         </div>
         <div className="w-full">
           <label htmlFor="description">Description</label>
-          <textarea
+          <input
             id="description"
             name="description"
-            rows={4}
+            type="text"
             maxLength={30}
             className="bg-base-200 p-2 pl-4 mt-2 w-full border border-gray-600 focus:outline-2 focus:outline-gray-500 focus:-outline-offset-1 rounded-md"
-            defaultValue={""}
           />
         </div>
         <button
+          disabled={isPending}
           type="submit"
-          className="ml-auto rounded-md px-4 py-2 font-semibold border border-gray-600 shadow-xs bg-base-300 hover:bg-gray-500 hover:text-gray-900"
+          className="ml-auto mt-4 rounded-md px-4 py-2 font-semibold border border-gray-600 shadow-xs bg-base-300 hover:bg-gray-500 hover:text-gray-900"
         >
           Add Todo
         </button>
       </div>
+      {error && <div className="text-red-500">{error}</div>}
     </form>
   );
 }
